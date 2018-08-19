8805d3952fb6e6ab84bc85d4d746084825a8d311
YuliyaBond <y.d.bondareva@inbox.ru>
2018-08-19 18:23:17 +0300

    add js

--- common.js
+++ common.js
@@ -1,48 +1,22 @@
-/*var toggle = function() {
-	var mydiv = document.getElementById('slid-wrap__item-slide--second');
-	var mydiv2 = document.getElementById('slid-wrap__item-slide--third');
-	var mydiv3 = document.getElementById('slid-wrap__item-slide--first');
-  
-	if (mydiv.style.display === 'none' || mydiv.style.display === ''){
-		mydiv.style.display = 'block';
-		mydiv3.style.display = 'none';
-	}
-	else
-		mydiv.style.display = 'none';  
-	
-	if (mydiv2.style.display === 'none' || mydiv.style.display === ''){
-		mydiv2.style.display = 'block';
-		mydiv3.style.display = 'none';
-	}
-	else
-    mydiv2.style.display = 'none';
-
-} 
-*/
-/*var _click = function () {
-	var b = 1;
-	return function (c) {
-		var a = document.getElementById("item" + b);
-			c == b && (a.style.display = "none" == a.style.display ? "" : "none");
-			c != b && (a.style.display = "none", a = document.getElementById("item" + c), a.style.display = "", b = c)
-		}
-}();
-
-window.onload = function() {
-
-    _click(1)
- }
-*/
-var elements = document.getElementsByClassName('.item-slide');
-
 var slides = document.querySelectorAll('.slider__content .item-slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide(n) {
  slides[currentSlide].className = 'item-slide';
- currentSlide = (currentSlide+1)%slides.length;
- 
+ //currentSlide = (currentSlide+1)%slides.length;
+ currentSlide = (n)%slides.length;
  slides[currentSlide].className = 'item-slide showing';
 }
 
+var toggle = function(){
+	var divs = document.querySelectorAll("input.indicator__button");
+    var handler = function() {
+        var index = this.getAttribute('data-index');
+        alert(index);
+			nextSlide(index);
+    }
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].setAttribute('data-index', i);
+        divs[i].onclick = handler; 
+    }
+}
